fbb1c9c46d01ca61faef1f082a1bca46f6b023bb
Nikita Bakaev <ya@nbakaev.ru>
2019-03-30 22:54:52 +0300

    fix youtube-dl #4

--- src/app/core/app-config.ts
+++ src/app/core/app-config.ts
@@ -5,7 +5,7 @@ export class AppConfig {
 
   public getServerBaseUrl() {
     // TODO: refactor
-    if (window.location.origin.indexOf("localhost") > 0) {
+    if (window.location.origin.indexOf("localhost") < 0) {
       return location.origin;
     }
     return `http://localhost:8080`;
