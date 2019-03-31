aeec8b34bb9594273a383cb4ff473dac81891f7d
Nikita Bakaev <ya@nbakaev.ru>
2019-03-31 13:32:59 +0300

    fix parse youtube video id

--- src/app/downloader/server-download-provider.service.ts
+++ src/app/downloader/server-download-provider.service.ts
@@ -13,11 +13,17 @@ export class ServerDownloadProviderService extends DownloadProviderService {
     super();
   }
 
+  private parseYoutubeVideoId(url: string): string {
+    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    const match = url.match(regExp);
+    return (match && match[7].length == 11) ? match[7] : url;
+  }
+
   download(url: string, type: string): Observable<DownloadStatus> {
     // type = 3 ; partialText
     // type = 4 : Response
     // https://github.com/angular/angular/blob/master/packages/common/http/src/xhr.ts
-    return this.httpClient.get(`${this.appConfig.getServerBaseUrl()}/api/v1/downloader/download_video/TtIN4_RZwZE/${type}`,
+    return this.httpClient.get(`${this.appConfig.getServerBaseUrl()}/api/v1/downloader/download_video/${this.parseYoutubeVideoId(url)}/${type}`,
       {observe: 'events', reportProgress: true, responseType: 'text'}).pipe(
       map(response => response as any),
       map((x: any) => {
